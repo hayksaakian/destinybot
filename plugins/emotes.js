24b11faf3c309f4c21f37fe15654005488633031
Peter SZTANOJEV <sztanpet@gmail.com>
2014-12-07 01:46:43 +0100

    preliminary penalizer plugin and anti-facespam plugin

---
+++ plugins/emotes.js
@@ -0,0 +1,75 @@
+var request = require("request");
+
+var p = function(core, config, state) {
+  var self = this;
+  this.core = core;
+  this.config = config;
+  this.state = state;
+
+  self.core.on("MSG", function(payload) {
+    var match = payload.data.match(self.state.regex);
+    if (!match || match.length < 6)
+      return;
+
+    self.core.emit("penalize.minor", payload.nick, "facespam");
+  });
+
+  self.core.on("!emotes", function(arg, payload) {
+    var now = Date.now()
+    if (now - state.lastmention > config.cooldown)
+      return;
+
+    self.core.say(payload.nick + ": /emotes or see " + config.linkurl + " also, don't forget that we have tab-autocomplete for emotes");
+    state.lastmention = now;
+  });
+
+  self.compileRegex = function() {
+    var self = this;
+    self.state.regex = new RegExp('(?:^|[\\s,\\.\\?!])(' + self.state.emoticons.join('|') + ')(?=$|[\\s,\\.\\?!])', 'gm');
+  }.bind(self);
+
+  self.refreshEmotes = function() {
+    request({
+      url: self.config.fetchurl,
+      json: true
+    }, function(err, response, data) {
+      if (err || response.statusCode !== 200) {
+        self.core.log("Emotes - could not get emotes, err: ", err, " code: ", response.statusCode);
+        return;
+      }
+
+      if (!data || !data.length) {
+        self.core.log("Emotes - data empty or not an array? ", data);
+        return;
+      }
+
+      self.state.emotes = data;
+      self.compileRegex();
+    });
+  }.bind(self);
+
+  // refresh emotes every two minutes
+  setInterval(self.refreshEmotes, 2 * 60 * 1000);
+};
+
+module.exports = {
+  init: p,
+  config: {
+    fetchurl: 'http://www.destiny.gg/chat/emotes.json',
+    linkurl: 'destiny.gg/emotes',
+    cooldown: 30 * 1000
+  },
+  state: {
+    lastmention: 0,
+    emotes: [
+      "Dravewin", "INFESTINY", "FIDGETLOL", "Hhhehhehe", "GameOfThrows",
+      "WORTH", "FeedNathan", "Abathur", "LUL", "Heimerdonger", "SoSad",
+      "DURRSTINY", "SURPRISE", "NoTears", "OverRustle", "DuckerZ", "Kappa",
+      "Klappa", "DappaKappa", "BibleThump", "AngelThump", "FrankerZ",
+      "BasedGod", "TooSpicy", "OhKrappa", "SoDoge", "WhoahDude",
+      "MotherFuckinGame", "DaFeels", "UWOTM8", "CallCatz", "CallChad",
+      "DatGeoff", "Disgustiny", "FerretLOL", "Sippy", "DestiSenpaii", "KINGSLY",
+      "Nappa", "DAFUK", "AYYYLMAO", "DANKMEMES", "MLADY", "SOTRIGGERED"
+    ]
+  }
+};
---
+++ plugins/penalize.js
@@ -0,0 +1,121 @@
+var extend = require("extend");
+
+var p = function(core, config, state) {
+  var self = this;
+  this.core = core;
+  this.config = config;
+  this.state = state;
+
+  self.core.on("penalize.minor", function(nick, reason) {
+    self.penalize("minor", nick, reason);
+  });
+
+  self.core.on("penalize.major", function(nick, reason) {
+    self.penalize("major", nick, reason);
+  });
+
+  self.core.on("penalize.mute", function(nick, duration, reason) {
+    self.core.send("MUTE", {
+      duration: duration * 1000000, // the chat server deals with nanoseconds, so multiply it up
+      data: nick
+    });
+
+    // format the duration as a timestamp, in the simplest way possible
+    var time = "";
+    var days = Math.floor(duration / (24 * 60 * 60 * 1000));
+    if (days > 0) {
+      time += days + " days ";
+      duration -= days * 24 * 60 * 60 * 1000;
+    }
+
+    var hours = Math.floor(duration / (60 * 60 * 1000));
+    if (hours > 0) {
+      time += hours + "h ";
+      duration -= hours * 60 * 60 * 1000;
+    }
+
+    var minutes = Math.floor(duration / (60 * 1000));
+    if (minutes > 0)
+      time = time + minutes + "m";
+
+    time = time.trim();
+    self.core.say(time + " " + nick + ": " + reason);
+  });
+
+  self.core.on("penalize.unmute", function(nick) {
+    self.core.send("UNMUTE", {
+      data: nick
+    });
+  });
+
+  self.penalize = function(type, nick, reason) {
+    var now = Date.now();
+    var settings = config[type];
+    var record = {
+      score: 0,
+      lastinfraction: 0,
+      numberofinfactions: 0
+    };
+
+    if (state.nicks[nick])
+      record = extend(record, state.nicks[nick]);
+
+    record.lastinfraction = now;
+    record.numberofinfactions += 1;
+
+    if (!record.score)
+      record.score = settings.def;
+    else
+      record.score = record.score * settings.mul;
+
+    state.nicks[nick] = record;
+    self.core.emit("penalize.mute", nick, record.score, reason);
+  };
+
+  self.decayScores = function(penalties, settings) {
+    var now = Date.now();
+    for (var nick in penalties) {
+      if (!penalties.hasOwnProperty(nick))
+        continue;
+
+      var record = penalties[nick];
+      if (!record.score || now - record.lastinfraction < settings.decayafter)
+        return;
+
+      // add it back, so that we can decrease it again after decayafter time passes
+      record.lastinfraction = now;
+      record.score = Math.round(record.score / settings.decayby);
+      if (record.score <= settings.def)
+        record.score = 0;
+
+    };
+  }.bind(self);
+
+  setInterval(function() {
+    self.decayScores(state.minor, config.minor);
+    self.decayScores(state.major, config.major);
+  }, 60 * 1000);
+};
+
+module.exports = {
+  init: p,
+  config,
+  {
+    minor: {
+      def: 60 * 1000, // default 1 minute mute
+      mul: 5, // every other infraction will multiply it by this much
+      decayafter: 60 * 60 * 1000, // penalties start decaying after an hour
+      decayby: 5 // the number of miliseconds is divided by this number after decaying
+    },
+    major: {
+      def: 5 * 60 * 1000, // 5 min
+      mul: 5,
+      decayafter: 24 * 60 * 60 * 1000, // 24h
+      decayby: 5
+    }
+  },
+  state: {
+    minor: {},
+    major: {}
+  }
+};
